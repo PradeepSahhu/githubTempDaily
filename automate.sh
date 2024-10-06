f2116aa1bf86d00e52769c591c1389883737f5a5
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-10-06 13:05:34 +0530

    tempgithub

---
+++ automate.sh
@@ -0,0 +1,23 @@
+#!/opt/homebrew/bin/bash   # shebang
+
+
+until_not_completed=true
+
+while [[ "$until_not_completed" == true ]]; 
+do
+	echo
+	echo "###########################################"
+	echo "################ Making the file ##########"
+	touch $FILENUMBER.txt
+	echo "File Successfully Created"
+	echo
+	echo "###########################################"
+	echo "###############Initiating the Github#######"
+        echo "!!! No message needed for github commit !!!"
+        #read githubMessage
+        git add .
+        git commit -m "temp"
+        git push -u origin main
+        until_not_completed=false
+done
+
