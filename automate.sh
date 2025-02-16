e8c4760ef08d6b768015869ac022037cdb3fb82d
Pradeep Sahu <official.pradeepsahu@gmail.com>
2025-02-16 12:22:55 +0530

    today command history

--- automate.sh
+++ automate.sh
@@ -8,8 +8,7 @@ do
 	echo
 	echo "###########################################"
 	echo "################ Making the file ##########"
-	touch "$FILENUMBER.txt"
-	history >> "$FILENUMBER.txt"
+	touch $FILENUMBER.txt
 	echo "File Successfully Created"
 	echo
 	echo "###########################################"
