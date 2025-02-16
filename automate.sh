49c11805d59652d3efd05f1be22d27a4125dd170
Pradeep Sahu <official.pradeepsahu@gmail.com>
2025-02-16 12:17:37 +0530

    today command history

--- automate.sh
+++ automate.sh
@@ -8,7 +8,7 @@ do
 	echo
 	echo "###########################################"
 	echo "################ Making the file ##########"
-	touch $FILENUMBER.txt
+	touch $FILENUMBER.txt > history
 	echo "File Successfully Created"
 	echo
 	echo "###########################################"
@@ -16,7 +16,7 @@ do
         echo "!!! No message needed for github commit !!!"
         #read githubMessage
         git add .
-        git commit -m "temp"
+        git commit -m "today command history"
         git push -u origin main
         until_not_completed=false
 done
